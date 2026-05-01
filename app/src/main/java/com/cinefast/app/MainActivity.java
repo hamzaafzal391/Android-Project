cf28ecb7d3e183168dbb765d386f514fa60951c5
Hamza Afzal <hamzacreed98@gmail.com>
2026-05-01 02:58:45 -0700

    add drawer navigatio0n with lagout

--- app/src/main/java/com/cinefast/app/MainActivity.java
+++ app/src/main/java/com/cinefast/app/MainActivity.java
@@ -1,14 +1,24 @@
 package com.cinefast.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +28,52 @@ public class MainActivity extends AppCompatActivity {
         Log.d("CineFAST", "MainActivity launched");
         Toast.makeText(this, "MainActivity launched", Toast.LENGTH_SHORT).show();
 
+        drawerLayout = findViewById(R.id.drawerLayout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close
+        );
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView nav = findViewById(R.id.navigationView);
+        nav.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_home) {
+                getSupportFragmentManager().popBackStack(null, androidx.fragment.app.FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                replaceRoot(new HomeFragment());
+            } else if (id == R.id.nav_my_bookings) {
+                getSupportFragmentManager().popBackStack(null, androidx.fragment.app.FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                replaceRoot(new MyBookingsFragment());
+            } else if (id == R.id.nav_logout) {
+                FirebaseAuth.getInstance().signOut();
+                SessionManagerV3.clear(this);
+                Intent i = new Intent(this, LoginActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(i);
+                finish();
+            }
+            drawerLayout.closeDrawers();
+            return true;
+        });
+
         if (savedInstanceState == null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.mainFragmentContainer, new HomeFragment())
-                    .commit();
+            nav.setCheckedItem(R.id.nav_home);
+            replaceRoot(new HomeFragment());
         }
+    }
+
+    private void replaceRoot(@NonNull androidx.fragment.app.Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.mainFragmentContainer, fragment)
+                .commit();
     }
 
     public void openSeatSelection(String movieName, String movieType) {
